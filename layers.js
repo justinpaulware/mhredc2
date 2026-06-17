609217b2b0241dfd9aa9085a78300ec91b8959b6
justinpaulware <77998303+justinpaulware@users.noreply.github.com>
2026-06-16 20:56:11 -0400

    layers.js

--- layers.js
+++ layers.js
@@ -4,7 +4,7 @@ const LAYERS = [
     name: "Opportunity Sites",
     type: "sheet",
 
-    url: "YOUR_GOOGLE_SHEET_CSV_URL",
+    url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOmFRs9WqWbsV18n0Ulujr-yNGiyEfFmiDzKQEXBuDBh-Cak-ZQdhczddXWLnRQcDk_1dmAawjtqb6/pub?output=csv",
 
     latField: "Latitude",
     lngField: "Longitude",
@@ -20,7 +20,7 @@ const LAYERS = [
     name: "Counties",
     type: "rest",
 
-    url: "YOUR_REST_GEOJSON_URL",
+    url: "https://services6.arcgis.com/EbVsqZ18sv1kVJ3k/arcgis/rest/services/NYS_Civil_Boundaries/FeatureServer/2/query?where=NAME='Westchester' OR NAME='Rockland' OR NAME='Sullivan' OR NAME='Putnam' OR NAME='Ulster' OR NAME='Orange' OR NAME='Dutchess' &outFields=*&f=geojson",
 
     paint: {
       fill: "#f59e0b",
